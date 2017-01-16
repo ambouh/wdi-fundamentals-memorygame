1d564408ffd1c8ed15d3b9ca372d10bf77c2da5c
ambouh <aphricanboi@gmail.com>
2017-01-16 15:55:29 -0500

    added a createCards function

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,24 @@ cardTwo = "queen",
 cardThree ="king", 
 cardFour ="king";
 
+var allCards = [cardOne, cardTwo, cardThree, cardFour];
+
 if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
 }
+
+var createCards = function(allCards){
+	var gameBoard = document.getElementById('game-board');
+	var newDiv;
+
+	for (var i = 0; i < allCards.length; i++) {
+		newDiv = document.createElement('div');
+		newDiv.className = "card";
+		gameBoard.appendChild(newDiv);
+	}
+
+}
+
+createCards(allCards);
